8fc00b1354d2a9bd4aec8ec9a41c5dd1cb53f68d
unknown <ramana.sri@gmail.com>
2025-10-16 00:15:11 +0530

    added comment

--- DSA_Patterns/Arrays/TwoPointers/RemoveDuplicatesFromSortedArray.py
+++ DSA_Patterns/Arrays/TwoPointers/RemoveDuplicatesFromSortedArray.py
@@ -1,3 +1,7 @@
+#26. Remove Duplicates from Sorted Array
+"""Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
+Then return the number of unique elements in nums."""
+# time complexity-O(n), space compleity-O(1)
 class Solution:
     def removeDuplicates(self, nums:list[int])->int:
         l=1
